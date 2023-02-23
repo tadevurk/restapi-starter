be5efbc8a3262827a249545dc5d9d06fde6f4f4c
tadevurk <683343@student.inholland.nl>
2023-02-23 14:26:38 +0100

    get,post,put, delete api

--- app/controllers/productcontroller.php
+++ app/controllers/productcontroller.php
@@ -18,6 +18,19 @@ class ProductController extends Controller
     public function getAll()
     {
         // this code seems to have been lost
+        try {
+            if (isset($_GET['offset']) && isset($_GET['limit'])) {
+                $offset = $_GET['offset'];
+                $limit = $_GET['limit'];
+            } else {
+                $offset = NULL;
+                $limit = NULL;
+            }
+            $products = $this->service->getAll($offset, $limit);
+            $this->respond($products);
+        } catch (Exception $e) {
+            $this->respondWithError(500, $e->getMessage());
+        }
     }
 
     public function getOne($id)
@@ -25,7 +38,13 @@ class ProductController extends Controller
         $product = $this->service->getOne($id);
 
         // we might need some kind of error checking that returns a 404 if the product is not found in the DB
-
+        // check if product exists
+        if ($product == null) {
+            // header("HTTP/1.0 404 Not Found");
+            // exit;
+            $this->respondWithError(404, "Product not found");
+            return;
+        }
         $this->respond($product);
     }
 
@@ -34,6 +53,7 @@ class ProductController extends Controller
         try {
             $product = $this->createObjectFromPostedJson("Models\Product");
             // something is missing. Shouldn't we update the product in the DB?
+            $product = $this->service->insert($product);
 
         } catch (Exception $e) {
             $this->respondWithError(500, $e->getMessage());
@@ -44,6 +64,37 @@ class ProductController extends Controller
 
     public function update($id)
     {
+        $updatedProduct = $this->service->getOne($id);
+
+        if ($product = null){
+            $this->respondWithError(404, "Product not found");
+            return;
+        }
         // There is no code here
-    } 
+        try{
+            $product = $this->createObjectFromPostedJson("Models\Product");
+            $this->service->update($product, $id);
+
+            $this->respond($updatedProduct);
+        } catch (Exception $e) {
+            $this->respondWithError(500, $e->getMessage()); 
+        }
+        $updatedProduct = $this->service->getOne($id);
+    }
+
+    public function delete($id){
+        $product = $this->service->getOne($id);
+
+        if ($product == null){
+            $this->respondWithError(404, "Product not found");
+            return;
+        }
+
+        try{
+            $this->service->delete($id);
+            $this->respond($product);
+        } catch (Exception $e) {
+            $this->respondWithError(500, $e->getMessage()); 
+        }
+    }
 }
--- app/public/index.php
+++ app/public/index.php
@@ -16,6 +16,7 @@ $router->setNamespace('Controllers');
 $router->get('/products', 'ProductController@getAll');
 $router->get('/products/(\d+)', 'ProductController@getOne');
 $router->put('/products/(\d+)', 'ProductController@update');
-
+$router->post('/products', 'ProductController@create');
+$router->delete('/products/(\d+)', 'ProductController@delete');
 // Run it!
 $router->run();
